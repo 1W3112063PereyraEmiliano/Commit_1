a6192b4401f4358b2d3426952ea0fa1bd320ff3f
Emiliano <112063@tecnicatura.frc.utn.edu.ar>
2021-07-01 21:32:31 -0300

    final_

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from datetime import datetime
 
 class Post(models.Model):
     # # author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     #author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    author = "yo"
+    author = "ID_EMSER"
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=datetime.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_date = datetime.now()
         self.save()
 
     def __str__(self):
